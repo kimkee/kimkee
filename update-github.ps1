04e92f6cf9a98e0083ffeb5c6a41bb09f0e708d4
KimKeeHyun <kimkee7@gmail.com>
2025-04-26 13:46:43 +0900

    d

---
+++ update-github.ps1
@@ -0,0 +1,19 @@
+# 파일 경로: C:\scripts\update-github.ps1
+
+$repoPath = "D:\workspace\kimkee"
+$updateFile = "$repoPath\update.json"
+$today = Get-Date -Format "yyyy-MM-dd"
+
+# JSON 파일 덮어쓰기
+@"
+{
+    "data": "$today"
+}
+"@ | Set-Content -Encoding UTF8 $updateFile
+
+# Git 작업
+cd $repoPath
+git pull
+git add update.json
+git commit -m "Update on $today"
+git push
